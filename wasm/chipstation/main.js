ceefdae5c25fbf6032f415446ce1a1da185a6dca
MrChip53 <chris@simoni.dev>
2024-07-02 12:10:23 -0500

    feat: enable dropping links to load roms
    text changes, remove log

--- wasm/chipstation/main.js
+++ wasm/chipstation/main.js
@@ -163,6 +163,17 @@ function attachKeyListeners() {
 }
 
 function attachRomUploadListeners() {
+  const handleFile = (file) => {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const arrayBuffer = e.target.result;
+      const uint8Array = new Uint8Array(arrayBuffer);
+      startRom(uint8Array);
+      document.body.style.backgroundColor = '#000';
+    };
+    reader.readAsArrayBuffer(file);
+  };
+
   const dragOverListener = (event) => {
      event.preventDefault();
      document.body.style.backgroundColor = 'green';
@@ -176,15 +187,22 @@ function attachRomUploadListeners() {
   const dropListener = (event) => {
     event.preventDefault();
     document.body.style.backgroundColor = '#00F';
-    const file = event.dataTransfer.files[0];
-    const reader = new FileReader();
-    reader.onload = function(e) {
-      const arrayBuffer = e.target.result;
-      const uint8Array = new Uint8Array(arrayBuffer);
-      startRom(uint8Array);
-      document.body.style.backgroundColor = '#000';
-    };
-    reader.readAsArrayBuffer(file);
+    if (event.dataTransfer.files.length > 0) {
+      const file = event.dataTransfer.files[0];
+      handleFile(file);
+    } else {
+      const text = event.dataTransfer.getData('text');
+      if (text.startsWith('http') || text.startsWith('data:')) {
+        fetch(text)
+          .then(response => response.arrayBuffer())
+          .then(buffer => {
+            const uint8Array = new Uint8Array(buffer);
+            startRom(uint8Array);
+          });
+      } else {
+        loadRomFromText(text);
+      }
+    }
   };
 
   document.addEventListener('dragover', dragOverListener);
@@ -212,12 +230,10 @@ function attachVisibilityListener() {
     if (document.hidden) {
       runningOnHide = !isPaused();
       if (runningOnHide) {
-        console.log("Pausing on hide");
         pause();
       }
     } else {
       if (runningOnHide) {
-        console.log("Resuming on show");
         resume();
       }
     }
